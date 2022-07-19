142152a4638f5b6d9d6930ac6c2b742254a87826
Gustavo Valmaña Villalonga <gustavo.shotokai@gmail.com>
2022-07-19 00:21:34 -0400

    user hace login por email

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,9 +4,8 @@ from simple_history.models import HistoricalRecords
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, username, email, name,last_name, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, email, name,last_name, password, is_staff, is_superuser, **extra_fields):
         user = self.model(
-            username = username,
             email = email,
             name = name,
             last_name = last_name,
@@ -18,14 +17,13 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
         return user
 
-    def create_user(self, username, email, name,last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, False, False, **extra_fields)
+    def create_user(self, email, name,last_name, password=None, **extra_fields):
+        return self._create_user(email= email, name= name,last_name= last_name, password= password, is_staff= False, is_superuser= False, **extra_fields)
 
-    def create_superuser(self, username, email, name,last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, True, True, **extra_fields)
+    def create_superuser(self, email, name,last_name, password=None, **extra_fields):
+        return self._create_user(email= email, name = name, last_name= last_name, password= password, is_staff= True, is_superuser= True, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length = 255, unique = True)
     email = models.EmailField('Correo Electrónico',max_length = 255, unique = True,)
     name = models.CharField('Nombres', max_length = 255, blank = True, null = True)
     last_name = models.CharField('Apellidos', max_length = 255, blank = True, null = True)
@@ -39,8 +37,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email','name','last_name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','last_name']
 
     def __str__(self):
         return f'{self.name} {self.last_name}'
